b4bf31b0c0c74a13f2dfed1077940da84a53a75d
yuo <2685245554@qq.com>
2024-12-11 20:46:42 +0800

    砧板剑攻击玩家崩溃bug修复

--- src/main/java/net/xiaoyang010/ex_enigmaticlegacy/Item/weapon/ManaitaSwordGod.java
+++ src/main/java/net/xiaoyang010/ex_enigmaticlegacy/Item/weapon/ManaitaSwordGod.java
@@ -2,17 +2,23 @@ package net.xiaoyang010.ex_enigmaticlegacy.Item.weapon;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import morph.avaritia.util.InfinityDamageSource;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
@@ -23,7 +29,6 @@ import net.xiaoyang010.ex_enigmaticlegacy.Init.ModTabs;
 import net.xiaoyang010.ex_enigmaticlegacy.Util.ColorText;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Method;
 import java.util.List;
 
 public class ManaitaSwordGod extends SwordItem {
@@ -42,8 +47,7 @@ public class ManaitaSwordGod extends SwordItem {
             target.setHealth(0.0F);
 
             // 如果目标是玩家，清空其食物值和经验值
-            if (target instanceof Player) {
-                Player player = (Player) target;
+            if (target instanceof Player player) {
                 player.getFoodData().setFoodLevel(0);
                 player.experienceLevel = 0;
             }
@@ -53,22 +57,22 @@ public class ManaitaSwordGod extends SwordItem {
 
     @Override
     public Component getName(ItemStack p_41458_) {
-        return Component.nullToEmpty(ColorText.GetColor1("砧板之刃[神]"));
+        return Component.nullToEmpty(ColorText.GetColor1(I18n.get("item.ex_enigmaticlegacy.manaita_sword")));
     }
 
     @Override
     public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> tooltip, TooltipFlag p_41424_) {
         tooltip.add(Component.nullToEmpty(""));
-        tooltip.add(Component.nullToEmpty(ColorText.getGray("在主手时:")));
-        tooltip.add(Component.nullToEmpty(ColorText.GetColor1("无限 ") + ColorText.GetGreen("攻击伤害")));
-        tooltip.add(Component.nullToEmpty(ColorText.GetGreen("1.6 攻击速度")));
+        tooltip.add(Component.nullToEmpty(ColorText.getGray(I18n.get("text.ex_enigmaticlegacy.manaita_sword.hand"))));
+        tooltip.add(Component.nullToEmpty(ColorText.GetColor1(I18n.get("text.ex_enigmaticlegacy.manaita_sword.infinity"))
+                + ColorText.GetGreen(I18n.get("text.ex_enigmaticlegacy.manaita_sword.damage"))));
+        tooltip.add(Component.nullToEmpty(ColorText.GetGreen(I18n.get("text.ex_enigmaticlegacy.manaita_sword.speed"))));
         super.appendHoverText(p_41421_, p_41422_, tooltip, p_41424_);
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        if (!level.isClientSide && level instanceof ServerLevel) {
-            ServerLevel serverLevel = (ServerLevel) level;
+        if (!level.isClientSide && level instanceof ServerLevel serverLevel) {
             Vec3 playerPos = player.position();
             double radius = 180000.0; // 设置雷电召唤的半径
 
@@ -82,7 +86,8 @@ public class ManaitaSwordGod extends SwordItem {
                 createRainLightning_blot(entity.getX(), entity.getY(), entity.getZ(), level);
                 // 使用雷电伤害源杀死生物
                 entity.die(DamageSource.mobAttack(player));
-                dropItem(entity, 10, true);
+                if (entity instanceof Player playerHurt)
+                    dropItem(playerHurt);
                 entity.hurt(DamageSource.LIGHTNING_BOLT, Float.MAX_VALUE);
                 entity.setHealth(0.0F); // 确保死亡
                 //这里确保死亡建议用setRemove()
@@ -107,56 +112,44 @@ public class ManaitaSwordGod extends SwordItem {
 
     @Override
     public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-        // 自定义攻击距离，模拟无限攻击距离
-        double maxDistance = 180000.0; // 设置一个极大的攻击距离
-        Vec3 playerPos = player.getEyePosition(1.0F);
-        Vec3 lookVec = player.getLookAngle();
-        Vec3 reachVec = playerPos.add(lookVec.scale(maxDistance));
-
-        // 定义一个 AABB 边界框，使用扩展的 reachVec
-        AABB boundingBox = new AABB(playerPos, reachVec);
-        List<LivingEntity> entities = player.level.getEntitiesOfClass(LivingEntity.class, boundingBox,
-                e -> e != player && e.distanceTo(player) <= maxDistance);
-
-        for (LivingEntity entity1 : entities) {
-            dropItem(entity1, 1, true);
-            dropItem(entity1, 1, true);
-            dropItem(entity1, 1, true);
-            entity1.die(DamageSource.mobAttack(player));
-            entity1.setHealth(0.0F);
-            entity1.setRemoved(Entity.RemovalReason.KILLED);
+        if (!player.level.isClientSide) {
+            if (entity instanceof LivingEntity living) {
+                if (entity instanceof Player playerHurt) {
+                    playerHurt.hurt(new InfinityDamageSource(player), Integer.MAX_VALUE);
+                    dropItem(playerHurt);
+                }else living.hurt(new InfinityDamageSource(player), Float.POSITIVE_INFINITY);
+                living.setHealth(-1.0f);
+                if (living.isAlive()){
+                    living.die(new InfinityDamageSource(player));
+                    living.kill();
+                    living.deathTime = 0;
+                }
+            }
         }
         return super.onLeftClickEntity(stack, player, entity);
     }
 
-//选中点到的实体（原来的）
-    //@Override
-    //public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-    //if (entity instanceof  LivingEntity entity1){
-    //dropItem(entity1,1,true);
-    //dropItem(entity1,1,true);
-    //dropItem(entity1,1,true);
-    //entity1.die(DamageSource.mobAttack(player));
-    //entity1.setHealth(0);//直接设置血量
-    //entity1.setRemoved(Entity.RemovalReason.KILLED);//这样会掉落物品,移除实体
-    //先看看能不能运行
-    //移到前面试试
-    //}
-    //return super.onLeftClickEntity(stack, player, entity);
-    //}
+    /**
+     * 被攻击玩家掉落背包物品
+     * @param playerHurt
+     */
+    private void dropItem(Player playerHurt){
+        InventoryMenu inventoryMenu = playerHurt.inventoryMenu;
+        for (Slot slot : inventoryMenu.slots) {
+            ItemStack item = slot.getItem();
+            if (!item.isEmpty()) {
+                playerHurt.drop(item, false);
+            }
+        }
 
-    private static void dropItem(LivingEntity mob, int i, boolean b) {
-        Class c = mob.getClass();
-        try {
-            //这里有一点需要主要
-            //m_7472_(Lnet/minecraft/world/damagesource/DamageSource;IZ)V dropCustomDeathLoot
-            //是方法混淆名，在作为一个模组加入游戏时用的就是这个
-            //现在是测试，方法经过反混淆，所以直接复制就行
-            //导出时记得改成混淆名:m_7472_
-            Method method = c.getDeclaredMethod("m_7472_");
-            method.invoke(mob, i, b);
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (ItemStack slot : playerHurt.getArmorSlots()) {
+            if (!slot.isEmpty()) {
+                playerHurt.drop(slot, false);
+            }
+        }
+        ItemStack offhandItem = playerHurt.getOffhandItem();
+        if (!offhandItem.isEmpty()) {
+            playerHurt.drop(offhandItem, false);
         }
     }
 
--- src/main/java/net/xiaoyang010/ex_enigmaticlegacy/Item/weapon/RealFinalSwordGod.java
+++ src/main/java/net/xiaoyang010/ex_enigmaticlegacy/Item/weapon/RealFinalSwordGod.java
@@ -3,17 +3,23 @@ package net.xiaoyang010.ex_enigmaticlegacy.Item.weapon;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import morph.avaritia.util.InfinityDamageSource;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
@@ -24,7 +30,6 @@ import net.xiaoyang010.ex_enigmaticlegacy.Init.ModTabs;
 import net.xiaoyang010.ex_enigmaticlegacy.Util.ColorText;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Method;
 import java.util.List;
 
 public class RealFinalSwordGod extends SwordItem {
@@ -54,22 +59,22 @@ public class RealFinalSwordGod extends SwordItem {
 
     @Override
     public Component getName(ItemStack p_41458_) {
-        return Component.nullToEmpty(ColorText.GetColor1("最终之剑[神]"));
+        return Component.nullToEmpty(ColorText.GetColor1(I18n.get("item.ex_enigmaticlegacy.real_final_sword")));
     }
 
     @Override
     public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> tooltip, TooltipFlag p_41424_) {
         tooltip.add(Component.nullToEmpty(""));
-        tooltip.add(Component.nullToEmpty(ColorText.getGray("在主手时:")));
-        tooltip.add(Component.nullToEmpty(ColorText.GetColor1("无限 ")+ColorText.GetGreen("攻击伤害")));
-        tooltip.add(Component.nullToEmpty(ColorText.GetGreen("1.6 攻击速度")));
+        tooltip.add(Component.nullToEmpty(ColorText.getGray(I18n.get("text.ex_enigmaticlegacy.manaita_sword.hand"))));
+        tooltip.add(Component.nullToEmpty(ColorText.GetColor1(I18n.get("text.ex_enigmaticlegacy.manaita_sword.infinity"))
+                + ColorText.GetGreen(I18n.get("text.ex_enigmaticlegacy.manaita_sword.damage"))));
+        tooltip.add(Component.nullToEmpty(ColorText.GetGreen(I18n.get("text.ex_enigmaticlegacy.manaita_sword.speed"))));
         super.appendHoverText(p_41421_, p_41422_, tooltip, p_41424_);
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        if (!level.isClientSide && level instanceof ServerLevel) {
-            ServerLevel serverLevel = (ServerLevel) level;
+        if (!level.isClientSide && level instanceof ServerLevel serverLevel) {
             Vec3 playerPos = player.position();
             double radius = 180000.0; // 设置雷电召唤的半径
 
@@ -80,23 +85,25 @@ public class RealFinalSwordGod extends SwordItem {
 
             // 对每个非玩家生物召唤彩色雷电并杀死它们
             for (LivingEntity entity : entities) {
-                createRainLightning_blot(entity.getX(),entity.getY(),entity.getZ(),level);
+                createRainLightning_blot(entity.getX(), entity.getY(), entity.getZ(), level);
                 // 使用雷电伤害源杀死生物
                 entity.die(DamageSource.mobAttack(player));
-                dropItem(entity,10,true);
+                if (entity instanceof Player playerHurt)
+                    dropItem(playerHurt);
                 entity.hurt(DamageSource.LIGHTNING_BOLT, Float.MAX_VALUE);
                 entity.setHealth(0.0F); // 确保死亡
                 //这里确保死亡建议用setRemove()
             }
-            List<ItemEntity> itemEntities = serverLevel.getEntitiesOfClass(ItemEntity.class,boundingBox);
-            for (ItemEntity itemEntity: itemEntities){
+            List<ItemEntity> itemEntities = serverLevel.getEntitiesOfClass(ItemEntity.class, boundingBox);
+            for (ItemEntity itemEntity : itemEntities) {
                 player.getInventory().add(itemEntity.getItem());
             }
         }
 
         return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
     }
-    private void createRainLightning_blot(double x,double y,double z,Level level){
+
+    private void createRainLightning_blot(double x, double y, double z, Level level) {
         if (level instanceof ServerLevel _level) {
             EntityRainBowLightningBlot entityToSpawn = ModEntities.LIGHTNING_BLOT.get().create(_level);
             entityToSpawn.moveTo(Vec3.atBottomCenterOf(new BlockPos(x, y, z)));
@@ -107,56 +114,44 @@ public class RealFinalSwordGod extends SwordItem {
 
     @Override
     public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-        // 自定义攻击距离，模拟无限攻击距离
-        double maxDistance = 180000.0; // 设置一个极大的攻击距离
-        Vec3 playerPos = player.getEyePosition(1.0F);
-        Vec3 lookVec = player.getLookAngle();
-        Vec3 reachVec = playerPos.add(lookVec.scale(maxDistance));
-
-        // 定义一个 AABB 边界框，使用扩展的 reachVec
-        AABB boundingBox = new AABB(playerPos, reachVec);
-        List<LivingEntity> entities = player.level.getEntitiesOfClass(LivingEntity.class, boundingBox,
-                e -> e != player && e.distanceTo(player) <= maxDistance);
-
-        for (LivingEntity entity1 : entities) {
-            dropItem(entity1, 1, true);
-            dropItem(entity1, 1, true);
-            dropItem(entity1, 1, true);
-            entity1.die(DamageSource.mobAttack(player));
-            entity1.setHealth(0.0F);
-            entity1.setRemoved(Entity.RemovalReason.KILLED);
+        if (!player.level.isClientSide) {
+            if (entity instanceof LivingEntity living) {
+                if (entity instanceof Player playerHurt) {
+                    playerHurt.hurt(new InfinityDamageSource(player), Integer.MAX_VALUE);
+                    dropItem(playerHurt);
+                }else living.hurt(new InfinityDamageSource(player), Float.POSITIVE_INFINITY);
+                living.setHealth(-1.0f);
+                if (living.isAlive()){
+                    living.die(new InfinityDamageSource(player));
+                    living.kill();
+                    living.deathTime = 0;
+                }
+            }
         }
         return super.onLeftClickEntity(stack, player, entity);
     }
 
-//选中点到的实体（原来的）
-    //@Override
-    //public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-    //if (entity instanceof  LivingEntity entity1){
-    //dropItem(entity1,1,true);
-    //dropItem(entity1,1,true);
-    //dropItem(entity1,1,true);
-    //entity1.die(DamageSource.mobAttack(player));
-    //entity1.setHealth(0);//直接设置血量
-    //entity1.setRemoved(Entity.RemovalReason.KILLED);//这样会掉落物品,移除实体
-    //先看看能不能运行
-    //移到前面试试
-    //}
-    //return super.onLeftClickEntity(stack, player, entity);
-    //}
+    /**
+     * 被攻击玩家掉落背包物品
+     * @param playerHurt
+     */
+    private void dropItem(Player playerHurt){
+        InventoryMenu inventoryMenu = playerHurt.inventoryMenu;
+        for (Slot slot : inventoryMenu.slots) {
+            ItemStack item = slot.getItem();
+            if (!item.isEmpty()) {
+                playerHurt.drop(item, false);
+            }
+        }
 
-    private static void dropItem(LivingEntity mob,int i,boolean b){
-        Class c = mob.getClass();
-        try {
-            //这里有一点需要主要
-            //m_7472_(Lnet/minecraft/world/damagesource/DamageSource;IZ)V
-            //是方法混淆名，在作为一个模组加入游戏时用的就是这个
-            //现在是测试，方法经过反混淆，所以直接复制就行
-            //导出时记得改成混淆名:m_7472_
-            Method method = c.getDeclaredMethod("m_7472_");
-            method.invoke(mob,i,b);
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (ItemStack slot : playerHurt.getArmorSlots()) {
+            if (!slot.isEmpty()) {
+                playerHurt.drop(slot, false);
+            }
+        }
+        ItemStack offhandItem = playerHurt.getOffhandItem();
+        if (!offhandItem.isEmpty()) {
+            playerHurt.drop(offhandItem, false);
         }
     }
 
